71dbf87d46d0990423180b87dfd5bafffc2831bd
Tuan <tw52100725@gmail.com>
2025-09-17 09:12:23 +0700

    Refactors data loading and standardizes customer IDs
    Moves datasets into a dedicated folder and centralizes data loading to improve organization and code reuse.
    
    Introduces a module for standardizing customer identifiers across sources, aiding in consistent merging and analysis.
    
    Removes redundant scripts and simplifies data preprocessing by consolidating logic.
    
    Improves maintainability and prepares for future expansion.

--- data_of_customer.py
+++ data_of_customer.py
@@ -1,5 +1,2 @@
-import pandas as pd
-
-df1 = pd.read_csv("online_retail.csv")
-df2 = pd.read_csv("retail_sales_dataset.csv")
+import src
 
---
+++ src.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+df1 = pd.read_csv("dataset/online_retail.csv")
+df2 = pd.read_csv("dataset/retail_sales_dataset.csv")
+df_cust = pd.read_csv("dataset/customer_dataset.csv")
