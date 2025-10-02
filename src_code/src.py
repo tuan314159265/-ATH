954334d482e5b10b66ed6aaa1535029765eeb544
Tuan <tw52100725@gmail.com>
2025-10-02 22:01:17 +0700

    Add cleaned dataset files

---
+++ src_code/src.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+df1 = pd.read_csv("dataset/online_retail.csv")
+df2 = pd.read_csv("dataset/retail_sales_dataset.csv")
+df_cust = pd.read_csv("dataset/customer_dataset.csv")
+
